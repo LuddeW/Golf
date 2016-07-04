434edeb9a19d66ebc87b8d0f275f99afe56b8a94
LuddeW <ludwig.wiktorsson@gmail.com>
2016-07-04 22:48:40 +0200

    Added 2player strokes

--- Managers/StrokeManager.cs
+++ Managers/StrokeManager.cs
@@ -22,10 +22,15 @@ namespace Golf.Managers
         {
             oldKeyboard = currentKeyboard;
             currentKeyboard = Keyboard.GetState();
-            if (currentKeyboard.IsKeyDown(Keys.Space) && !oldKeyboard.IsKeyDown(Keys.Space))
+            if (currentKeyboard.IsKeyDown(Keys.RightControl) && !oldKeyboard.IsKeyDown(Keys.RightControl))
             {
                 ballManager.player1Ball.velocity.X = cursorManager.player1Cursor.pos.X / 100;
                 ballManager.player1Ball.velocity.Y = -cursorManager.player1Cursor.pos.Y / 100;
+            }
+            if (currentKeyboard.IsKeyDown(Keys.LeftControl) && !oldKeyboard.IsKeyDown(Keys.LeftControl))
+            {
+                ballManager.player2Ball.velocity.X = cursorManager.player2Cursor.pos.X / 100;
+                ballManager.player2Ball.velocity.Y = -cursorManager.player2Cursor.pos.Y / 100;
             }
         }
     }
