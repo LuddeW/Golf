1a058f50ffeb2b259f2aea3d3bfd11e715e0565a
LuddeW <ludwig.wiktorsson@gmail.com>
2016-07-04 16:51:35 +0200

    The ball gets speed in X-axis when you hit space :ocean:

--- Managers/BallManager.cs
+++ Managers/BallManager.cs
@@ -3,6 +3,7 @@ using Golf.GameObjects.Tiles;
 using Golf.Map;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +38,15 @@ namespace Golf.Managers
                 {
                     player1Ball.pos.Y -= 1;
                 }
+                if (player1Ball.hitbox.Intersects(tile.hitbox))
+                {
+                    player2Ball.pos.Y -= 1;
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                player1Ball.pos.Y -= 2;
+                player1Ball.pos.X += 2;
             }
             player1Ball.Update();
             player2Ball.Update();
